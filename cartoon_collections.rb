7c16af31ead02feb180c786cb1c3c8b60f201072
Derek Townsend <derekjavon2@gmail.com>
2019-05-09 04:16:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,9 +27,11 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  itemIndex=-1
   cheese_types.each do |cheese|
     if array.includes?(cheese)
       itemIndex=array.index(cheese)
+
     end
   end
 
