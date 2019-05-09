39f1f6ddf40b9abcb1920d99f167ad29c3b1e344
Derek Townsend <derekjavon2@gmail.com>
2019-05-09 04:04:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,11 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |word|
+    if word.length>4
+
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
