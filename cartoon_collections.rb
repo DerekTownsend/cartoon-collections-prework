4194c0091bce36958b881b4943e930073d0156bf
Derek Townsend <derekjavon2@gmail.com>
2019-05-09 04:14:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,13 @@ def long_planeteer_calls(array)# code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if array.includes?(cheese)
+      itemIndex=array.index(cheese)
+    end
+  end
+
 end
